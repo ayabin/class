8ad7969854f587a64bef93957e6c19695bfbfa52
ayabin <ayabin.jp@gmail.com>
2018-03-26 11:45:05 +0900

    Update example.php

--- example.php
+++ example.php
@@ -8,7 +8,7 @@ define("SERVER_DIR_PATH","xxxx/xxxx/");
 
 $file="test.php";
 
-$obj=new ftp(FTP_UPLOAD_SERVER,FTP_UPLOAD_USER,FTP_UPLOAD_PASS);
+$obj=new ftp(FTP_SERVER,FTP_USER,FTP_PASS);
 $obj->local_dir_path=LOCAL_DIR_PATH;
 $obj->server_dir_path=SERVER_DIR_PATH;
 $obj->upload($file);
